46f57127982fa718e266bb4c7e1aeeceab25337c
Tim Allhoff <42519781+hivife@users.noreply.github.com>
2022-03-08 21:38:51 +0100

    results added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Tue Feb  1 14:25:03 2022
 import networkx as nx
 import re
 import os
+import csv
 
 
 
@@ -15,7 +16,8 @@ import os
 # functions
 
 def read_gv_file(file_path):
-	bdd = nx.Graph()
+	print("reading file")
+	bdd = nx.DiGraph()
 	file = open(file_path)
 	for line in file:
 		line_is_edge = re.search("^\".+\" -> \".+\"(;| \[style = dashed\];| \[style = solid\];)$",line)
@@ -43,13 +45,16 @@ def read_gv_file(file_path):
 			#print(edge_weight)
 			bdd.add_edge(first_node,second_node,weight = edge_weight)
 			
+			
 		if(line_is_label_true):
 			nodes = line.split(" ")
 			first_node = nodes[0].replace(" ","")
+			first_node = nodes[0].replace("\"","")
 			bdd.output_node_true = first_node
 		if(line_is_label_false):
 			nodes = line.split(" ")
 			first_node = nodes[0].replace(" ","")
+			first_node = nodes[0].replace("\"","")
 			bdd.output_node_false = first_node
 	
 	
@@ -60,64 +65,155 @@ def read_gv_file(file_path):
 
 
 def print_results(result , output_file , path):
-	file = open(output_file , "w")
-	file.write(path + "\n")
+	file = open(output_file , "a")
+	#file.write(path + "\n")
+	#for entry in result:
+	#	file.write(str(entry))
+	#	file.write("\n")
+	csv.writer(file).writerow(("file",path))
 	for entry in result:
-		file.write(str(entry))
-		file.write("\n")
+		csv.writer(file).writerow(entry)
+		#file.write("\n")
+	
+	
+	
 	
 	
 	file.close()
-	print(result)
+	#print(result)
 
 
 
 def analyze_bdd(bdd):
 	result = []
-	result.append(("Number of edges:",bdd.number_of_edges()))
-	result.append(("Number of nodes:",bdd.number_of_nodes()))
-	result.append(("root:",bdd.root_node))
-	result.append(("Output node false:",bdd.output_node_false))
-	result.append(("Output node true:",bdd.output_node_true))
-	result.append(("Average clustering:",nx.average_clustering(bdd)))
-	result.append(("Maximal Cliques:",nx.graph_number_of_cliques(bdd)))
-	#result.append(("Small-world coefficient:",nx.omega(bdd)))
-	#result.append(("Gerneralized Degree:",nx.generalized_degree(bdd)))
+	
+	
+	# General Information
+	
+	print("processing general information")
+	result.append(("Number of edges",bdd.number_of_edges()))
+	result.append(("Number of nodes",bdd.number_of_nodes()))
+	#result.append(("Root node",bdd.root_node))
+	#result.append(("Output node false",bdd.output_node_false))
+	#result.append(("Output node true",bdd.output_node_true))
+	
+	
+	# Clustering
+	print("processing cluster information")
+	result.append(("Average clustering",nx.average_clustering(bdd)))
+	#result.append(("Maximal Cliques",nx.graph_number_of_cliques(bdd)))
+	#result.append(("Gerneralized Degree",nx.generalized_degree(bdd)))
+	
+
+	temp = centrality_helper_function(nx.betweenness_centrality(bdd))
+	max_value = temp[0]
+	avg_value = temp[1]
+	result.append(("Betweenness centrality max. value",max_value))
+	result.append(("Betweenness centrality avg. value",avg_value))
+	
+	temp = centrality_helper_function(nx.closeness_centrality(bdd))
+	max_value = temp[0]
+	avg_value = temp[1]
+	result.append(("Closeness centrality max. value",max_value))
+	result.append(("Closeness centrality avg. value",avg_value))
+	
+	temp = centrality_helper_function(nx.degree_centrality(bdd))
+	max_value = temp[0]
+	avg_value = temp[1]
+	result.append(("Degree centrality max. value",max_value))
+	result.append(("Degree centrality avg. value",avg_value))
+	
+	#print(nx.betweenness_centrality(bdd))
+	#print(nx.closeness_centrality(bdd))
+	#print(nx.degree_centrality(bdd))
+	
+	
+	
+
+	#print(nx.k_core(bdd,3).size())
+	
+	
+
+	#paths
+	print("processing path information")
+	all_paths_false = nx.all_simple_edge_paths(bdd, bdd.root_node, bdd.output_node_false)
+	false_counter = 0
+	sum_false = 0
+	longest_path_false = 0
+	for path in all_paths_false:
+ 			path_length = len(path)
+ 			sum_false += path_length
+ 			false_counter += 1
+ 			if(longest_path_false < path_length): longest_path_false = path_length
+ 			
+	all_paths_true = nx.all_simple_edge_paths(bdd, bdd.root_node , bdd.output_node_true)
+	true_counter = 0
+	sum_true = 0
+	longest_path_true = 0
+	for path in all_paths_true:
+ 			path_length = len(path)
+ 			sum_true += path_length
+ 			true_counter += 1
+ 			if(longest_path_true < path_length): longest_path_true = path_length
+ 			
+	characteristic_path_length = (sum_false + sum_true) / (true_counter + false_counter)
+	characteristic_path_length_false = sum_true / true_counter
+	characteristic_path_length_true = sum_false  /  false_counter
+ 	
+	result.append(("Characteristic path length",characteristic_path_length))	
+	result.append(("Characteristic path length for output false",characteristic_path_length_false))
+	result.append(("Characteristic path length for output true",characteristic_path_length_true))
+	result.append(("Number of paths from root to output true",sum_true))	
+	result.append(("Number of paths from root to output false",sum_false))	
+	result.append(("Number of paths from root to any output",sum_true + sum_false))	
+	result.append(("Longest path from root to true",longest_path_true))	
+	result.append(("Longest path from root to false",longest_path_false))	
+ 	 	
+	#result.append(("Small-world coefficient",nx.omega(bdd)))
+ 
 	return result
 
 
 
+def centrality_helper_function(input_list):
+	values = input_list
+	max_value = 0
+	avg_value = 0
+	sum = 0
+	
+	for node in values:
+		current_value = values[node]
+		if(current_value > max_value):
+			max_value =  current_value
+		sum += current_value
+		
+	avg_value = sum / len(values)
+	
+	return (max_value,avg_value)
+
 
 #main
 
-input_folder = "data/"
-output_file = "output.txt"
-
-#for folder in os.listdir(input_folder):
-	#print(os.listdir(input_folder + folder))
-	
-
-path = "bmc_tutorial.gv"
+input_folder = "data_small/"
+output_file = "output.csv"
+#path = "bmc_tutorial.gv
 #path = "data/brp/brp.gv"
 
-current_bdd = read_gv_file(path)
 
+# clear file
+f = open("output.csv","w")
+f.close()
 
-result_list = analyze_bdd(current_bdd)
-
-
-print_results(result_list,output_file,path) 
-
-print(list(current_bdd.nodes))
-
-
-
-
-
-
-
-
+for folder in os.listdir(input_folder):
+	for file in os.listdir(input_folder + folder):
+		print("starting:" + input_folder + folder + "/" + file)
+		path = input_folder + folder + "/" + file
+		current_bdd = read_gv_file(path)
+		result_list = analyze_bdd(current_bdd)
+		print_results(result_list,output_file,path) 
+		print("completed:" + input_folder + folder + "/" + file)
+	
 
 
 
-
+#print(list(current_bdd.edges))
